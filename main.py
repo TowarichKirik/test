6280543e8dccd57fc1a219b9a22e28e6a5d65318
TowarichKirik <theender.287@mail.ru>
2025-05-26 14:44:03 +0300

    начало

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import sqlite3
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
+
+
 cursor.execute("""CREATE TABLE IF NOT EXISTS users(
 id INT PRIMARY KEY AUTOINCREMENT,
 name TEXT,
 login TEXT,
 password TEXT,
-Account REAL)
+Account REAL
+)
 """)
+
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS credit(
+id INT PRIMARY KEY AUTOINCREMENT,
+userId INT,
+Amount REAL,
+PPY REAL,
+AdInfo TEXT
+)
+""")
+

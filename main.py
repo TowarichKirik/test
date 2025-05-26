a800c61cd77e1dc02320935aeff3383b0a7e5549
TowarichKirik <theender.287@mail.ru>
2025-05-26 17:22:01 +0300

    Изменил INT на INTEGER в связи с багами

--- main.py
+++ main.py
@@ -24,3 +24,31 @@ AdInfo TEXT
 )
 """)
 
+
+# Операции
+def Operations(num):
+    def checkPassword(password):
+        if len(password) < 8:
+            print("Ваш пароль меньше 8 символов!")
+            return True
+        return False
+    #Создание аккаунта
+    def createAccount():
+        name = input("Ваше ФИО: ")
+        login = input("Ваш логин: ")
+
+        password = input("Ваш пароль: ")
+        while checkPassword(password):
+            password = input("Введите пароль повторно: ")
+        if len(password) < 8:
+            print("Ваш пароль меньше 8 символов!")
+            password = input("Ваш пароль: ")
+            if '0123456789' not in password:
+                print("Ваш пароль должен содержать не менее 1 цифры")
+                password = input("Ваш пароль: ")
+        cursor.execute(f"""INSERT INTO users (name, login, password, Account) VALUES ('{name}', '{login}', '{password}', 1000);""")
+        conn.commit()
+
+    if num == 1:
+        createAccount()
+Operations(1)
